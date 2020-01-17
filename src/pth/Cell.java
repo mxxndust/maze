e268c5a2a60b4815ebe49debc4e00bd98ac31d83
Dina Shabakova <mistywyatt@yandex.ru>
2020-01-18 01:38:58 +0400

    Change.

---
+++ src/pth/Cell.java
@@ -0,0 +1,36 @@
+package pth;
+
+public class Cell {
+	private boolean s;
+	private boolean u;
+	private boolean z;
+	private boolean v;
+	
+	public boolean isS() {
+		return s;
+	}
+	public void setS(boolean s) {
+		this.s = s;
+	}
+	public boolean isU() {
+		return u;
+	}
+	public void setU(boolean u) {
+		this.u = u;
+	}
+	public boolean isZ() {
+		return z;
+	}
+	public void setZ(boolean z) {
+		this.z = z;
+	}
+	public boolean isV() {
+		return v;
+	}
+	public void setV(boolean v) {
+		this.v = v;
+	}
+
+	
+
+}
--- src/pth/Maze.java
+++ src/pth/Maze.java
@@ -1,15 +1,69 @@
 package pth;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Maze {
 
 	public static void main(String[] args) {
-		Maze graph = new Maze();
-		graph.run();
+		Maze maze = new Maze();
+		maze.run();
 
 	}
 	
 	public void run() {
+		String p = "WRWWLWWLWWLWLWRRWRWWWRWWRWLW";
+		String o = "WWRRWLWLWWLWWLWWRWWRWWLW";
 		
+		int angle = 0;
+		
+		/*if (angle == 0) {
+			List<List<String>> row = new ArrayList<>();
+			//for(List<String> s : row) {
+			for(int n=0; n < row.size(); ++n) {
+				ArrayList <String> col = new ArrayList<>();
+				//for(String st : col) {
+				for(int m=0; m < row.size(); ++m) {
+					
+				}
+			}
+		}*/
+		
+		for (int i = 0; i < p.length()-2; ++i) {
+			if (p.valueOf(i).equals('W')) {
+				if (angle == 180) {
+					
+				}
+				else if (angle == 270) {
+					
+				}
+				else if (angle == 360) {
+					
+				}
+				else if (angle == 90) {
+					
+				}
+				
+				//Cell currentCell = row.get().get();
+			}
+			else if (p.valueOf(i).equals('L')) {
+				if (angle == 0) {
+					angle = 270;
+				}
+				else {
+					angle-=90;
+				}
+			}
+			else if (p.valueOf(i).equals('R')) {
+				if (angle == 270) {
+					angle = 0;
+				}
+				else {
+					angle+=90;
+				}
+			}
+		}
 	}
 
 }

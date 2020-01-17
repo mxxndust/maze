62bf9de0a80ba41c6ee4069f145b404da2dd0695
Dina Shabakova <mistywyatt@yandex.ru>
2020-01-18 02:49:16 +0400

    Update.

--- src/pth/Maze.java
+++ src/pth/Maze.java
@@ -17,6 +17,8 @@ public class Maze {
 		String o = "WWRRWLWLWWLWWLWWRWWRWWLW";
 		
 		int angle = 0;
+		//int n = 0;
+		//int m = 0;
 		
 		/*if (angle == 0) {
 			List<List<String>> row = new ArrayList<>();
@@ -30,7 +32,7 @@ public class Maze {
 			}
 		}*/
 		
-		for (int i = 0; i < p.length()-2; ++i) {
+		/*for (int i = 0; i < p.length()-2; ++i) {
 			if (p.valueOf(i).equals('W')) {
 				if (angle == 180) {
 					
@@ -61,6 +63,99 @@ public class Maze {
 				}
 				else {
 					angle+=90;
+				}
+			}
+		}*/
+		
+		List<List<String>> row = new ArrayList<>();
+		//for(List<String> s : row) {
+		for(int n = 0; n < row.size(); n++) {
+			ArrayList <String> col = new ArrayList<>();
+			//for(String st : col) {
+			for(int m = 0; m < col.size(); m++) {
+				for (int i = 0; i < p.length()-2; ++i) {
+					if (p.valueOf(i).equals('W')) {
+						if (angle == 180) {
+							if (row.get(n).get(m) == null) {
+								//row.get(n).get(m).add("s");
+								row.get(n).set(m, "s");
+							}
+							else {
+								//col.set(m, col.get(m)+"s");
+								row.get(n).set(m, col.get(m)+"s");
+							}
+						n++;
+						}
+						else if (angle == 270) {
+							if (m == 0) {
+								if (row.get(n).get(m) == null) {
+									row.get(n).set(m, "w");
+								}
+								else {
+									row.get(n).set(m, col.get(m)+"w");
+								}
+							m=0;
+							}
+							else {
+								if (row.get(n).get(m) == null) {
+									row.get(n).set(m, "w");
+								}
+								else {
+									row.get(n).set(m, col.get(m)+"w");
+								}
+							m--;
+							}
+						}
+						else if (angle == 0) {
+							if (n == 0) {
+								if (row.get(n).get(m) == null) {
+									row.get(n).set(m, "n");
+								}
+								else {
+									row.get(n).set(m, col.get(m)+"n");
+								}
+							n=0;
+							}
+							else {
+								if (row.get(n).get(m) == null) {
+									row.get(n).set(m, "n");
+								}
+								else {
+									row.get(n).set(m, col.get(m)+"n");
+								}
+							n--;
+							}
+						}
+						else if (angle == 90) {
+							if (row.get(n).get(m) == null) {
+								//row.get(n).get(m).add("s");
+								row.get(n).set(m, "e");
+							}
+							else {
+								//col.set(m, col.get(m)+"s");
+								row.get(n).set(m, col.get(m)+"e");
+							}
+						m++;
+						}
+						
+						//Cell currentCell = row.get().get();
+					}
+					else if (p.valueOf(i).equals('L')) {
+						if (angle == 0) {
+							angle = 270;
+						}
+						else {
+							angle-=90;
+						}
+					}
+					else if (p.valueOf(i).equals('R')) {
+						if (angle == 270) {
+							angle = 0;
+						}
+						else {
+							angle+=90;
+						}
+					}
 				}
 			}
 		}

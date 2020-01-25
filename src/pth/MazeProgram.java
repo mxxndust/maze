5089660bea3d3bbf5bb952bbdca7874a46475bbd
Dina Shabakova <mistywyatt@yandex.ru>
2020-01-26 01:50:04 +0400

    add writer

--- src/pth/MazeProgram.java
+++ src/pth/MazeProgram.java
@@ -1,8 +1,10 @@
 package pth;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,25 +19,35 @@ public class MazeProgram {
 	public void run() {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("D:\\small-test.in.txt"));
+			//BufferedReader br = new BufferedReader(new FileReader("D:\\large-test.in.txt"));
+			BufferedWriter bw = new BufferedWriter(new FileWriter("D:\\small.output.txt"));
+			//BufferedWriter bw = new BufferedWriter(new FileWriter("D:\\large.output.txt"));
 			String s;
 			int ci = Integer.parseInt(br.readLine());
 			for (int i = 0; i < ci; i++) {
-				System.out.println("Case #"+ (i+1) +":");
+				//System.out.println("Case #"+ (i+1) +":");
 		    	String[] words = br.readLine().split("\\s");
 		    	String one = words[0];
 		    	String two = words[1];
 		    	Maze maze = new Maze();
 		    	List<String> l = new ArrayList<>();
 				l = maze.lab(one,two);
-				for (int j = 0; j < l.size(); j++) {
+				/*for (int j = 0; j < l.size(); j++) {
 					   System.out.println(l.get(j));
+				}*/
+	            bw.write("Case #"+ (i+1) +":"+ "\n");
+	            for (int j = 0; j < l.size(); j++) {       	
+	            	bw.write(l.get(j) + "\n");
 				}
+	            bw.flush();
 		    }
+			System.out.println("Готово!");
+			 br.close();
+	         bw.close();
 		} catch (FileNotFoundException e) {
 			System.out.println("Ошибка!");
 			//e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
